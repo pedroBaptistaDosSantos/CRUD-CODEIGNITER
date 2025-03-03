9cd81810a90440b9daf5bc45afce58440cc360f5
pedroBaptistaDosSantos <pedrobaptista.dev@gmail.com>
2025-03-03 09:11:10 -0300

    Cria migração para a tabela de clientes

---
+++ app/Database/Migrations/2025-03-03-115828_CreateClientsTable.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class CreateClientsTable extends Migration
+{
+    public function up()
+    {
+        $this->forge->addField([
+            'id' => [
+                'type'           => 'INT',
+                'constraint'     => 11,
+                'unsigned'       => true,
+                'auto_increment' => true,
+            ],
+            'cpf_cnpj' => [
+                'type'       => 'VARCHAR',
+                'constraint' => 14, // CPF: 11, CNPJ: 14
+                'unique'     => true,
+            ],
+            'nome_razao_social' => [
+                'type'       => 'VARCHAR',
+                'constraint' => 255,
+            ],
+            'created_at' => [
+                'type' => 'DATETIME',
+                'null' => true,
+            ],
+            'updated_at' => [
+                'type' => 'DATETIME',
+                'null' => true,
+            ],
+        ]);
+
+        $this->forge->addKey('id', true);
+        $this->forge->createTable('clients');
+    }
+
+    public function down()
+    {
+        $this->forge->dropTable('clients');
+    }
+}
